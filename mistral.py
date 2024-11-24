b80a4ce54f3be002f360a6284f6728f630b8f015
Neel Munot <neelmunot2003@gmail.com>
2024-11-24 18:10:54 +0530

    hugging face login added

--- mistral.py
+++ mistral.py
@@ -1,12 +1,8 @@
-from huggingface_hub import snapshot_download
+from huggingface_hub import snapshot_download, login
 from pathlib import Path
 
-def download_model(repo_id, save_directory, allow_patterns):
-    snapshot_download(repo_id=repo_id, allow_patterns=allow_patterns, local_dir=save_directory)
+login(token="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")
+mistral_models_path = Path.home().joinpath('mistral_models', 'Nemo-Instruct')
+mistral_models_path.mkdir(parents=True, exist_ok=True)
 
-if __name__ == "__main__":
-    repo_id = "mistralai/Mistral-Nemo-Instruct-2407"
-    save_directory = Path.home().joinpath('mistral_models', 'Nemo-Instruct')
-    save_directory.mkdir(parents=True, exist_ok=True)
-    allow_patterns = ["params.json", "consolidated.safetensors", "tekken.json"]
-    download_model(repo_id, save_directory, allow_patterns)
+snapshot_download(repo_id="mistralai/Mistral-Nemo-Instruct-2407", allow_patterns=["params.json", "consolidated.safetensors", "tekken.json"], local_dir=mistral_models_path)
